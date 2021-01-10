6759931c15d6da6328c8b06d5392d73efe6c9668
Masum <radians86@yahoo.in>
2021-01-10 19:57:32 +0530

    trials

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var num = document.querySelector("#textbox");
-var btn_translate = document.querySelector("#textbox");
-var outputDiv = document.querySelector("#");
+var btn_translate = document.querySelector("#btn");
+var outputDiv = document.querySelector("#output");
 var i = 0;
 var x = 0;
 function isPalindrome() {
-  var ans = num.replace("/", "");
-  var ans1 = parseInt(num);
-  var orgin = ans1;
-  while (ans1 != 0) {
-    var rem = num % 10;
+  var ans = num.value;
+  var ans2 = parseInt(ans.split("/", "").join(""));
+  console.log(ans2);
+  var orgin = ans2;
+  while (ans2 != 0) {
+    var rem = ans2 % 10;
 
     x = x * 10 + rem;
 
-    num = num / 10;
+    ans2 = ans2 / 10;
   }
-  if (origin === ans1) {
+  if (origin === x) {
     alert("Your birthday is an Palindrome!");
+  } else {
+    alert("Bhag madarchod");
   }
 }
 
