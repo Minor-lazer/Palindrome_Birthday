33cce5118f32e73505e5c34b81c9145a80f43342
Masum <radians86@yahoo.in>
2021-01-10 12:07:18 +0530

    main logic

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var num = document.querySelector("#textbox");
 var btn_translate = document.querySelector("#textbox");
+var outputDiv = document.querySelector("#");
 var i = 0;
+var x = 0;
 function isPalindrome() {
   var ans = num.replace("/", "");
   var ans1 = parseInt(num);
-  while (num != 0) {
+  var orgin = ans1;
+  while (ans1 != 0) {
     var rem = num % 10;
 
     x = x * 10 + rem;
 
     num = num / 10;
+  }
+  if (origin === ans1) {
+    alert("Your birthday is an Palindrome!");
   }
 }
 
